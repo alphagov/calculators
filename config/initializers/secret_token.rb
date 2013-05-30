fec708fce3b1128e54e15ec3161cd087b1506370
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2013-05-30 10:53:27 +0000

    Blank Rails 3.2.13 app
    rails new calculators -T -J -O

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Calculators::Application.config.secret_token = '9d7d2b0f5e683258253e0d96d7944de860e9a0ccd61095943260906e6f6fa6d4d7a44d7d0f6157ae4c5494e5546ef4deb41c5fdf9c75f5abc1ef6c1f9981e9a3'
