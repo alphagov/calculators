6aeac0374aaff5ffbe776a3e22efa21818b85a0f
Jack Franklin <jack@jackfranklin.net>
2013-06-03 17:31:37 +0100

    Bootstrap initial calculator form and controller

--- app/controllers/child_benefit_tax_controller.rb
+++ app/controllers/child_benefit_tax_controller.rb
@@ -1,4 +1,11 @@
 class ChildBenefitTaxController < ApplicationController
   def landing
+    @calculator = ChildBenefitTaxCalculator.new(params)
+    @show_new_child_form = params[:commit] == "Add a new starting child"
+    @show_old_child_form = params[:commit] == "Add a new stopping child"
+
+    if params[:commit] == "Go"
+      @calculator = ChildBenefitTaxCalculator.new(params)
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Calculators::Application.routes.draw do
-
-  get "/child-benefit-tax-calculator" => "child_benefit_tax#landing", :format => false
+  get "/child-benefit-tax-calculator" => "child_benefit_tax#landing"
+  post "/child-benefit-tax-calculator" => "child_benefit_tax#landing"
 end
