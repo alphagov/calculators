c7814996c0ff8ff36c9fc07e0dffe7ad5ba72e30
Jack Franklin <jack@jackfranklin.net>
2013-06-04 16:34:23 +0100

    Update specs to reflect HTML changes

--- app/controllers/child_benefit_tax_controller.rb
+++ app/controllers/child_benefit_tax_controller.rb
@@ -3,6 +3,10 @@ class ChildBenefitTaxController < ApplicationController
   end
 
   def main
+    unless params[:year]
+      redirect_to "/child-benefit-tax-calculator"
+    end
+
     @calculator = ChildBenefitTaxCalculator.new(params)
     @show_new_child_form = params[:commit] == "Add a new starting child"
     @show_old_child_form = params[:commit] == "Add a new stopping child"
--- spec/features/child_benefit_tax_calculator_spec.rb
+++ spec/features/child_benefit_tax_calculator_spec.rb
@@ -3,32 +3,37 @@ require 'spec_helper'
 
 feature "Child Benefit Tax Calculator" do
 
-  before(:each) do
-    visit "/child-benefit-tax-calculator"
-  end
-
   it "should have a placeholder landing page" do
+    visit "/child-benefit-tax-calculator"
     within "header.page-header" do
       page.should have_content("Estimate your High Income Child Benefit Tax Charge")
     end
   end
 
+  it "should redirect to the landing page if no tax year exists" do
+    visit "/child-benefit-tax-calculator/main"
+    current_path.should == "/child-benefit-tax-calculator"
+
+  end
+
   it "should store the tax year when the user clicks it" do
+    visit "/child-benefit-tax-calculator"
     click_link "Click if you pay tax on Child Benefit for the tax year 2012 to 2013"
     page.should have_content("for 2012 to 2013")
   end
 
   describe "Calculating the results for 2012-13" do
     before(:each) do
+      visit "/child-benefit-tax-calculator"
       click_link "Click if you pay tax on Child Benefit for the tax year 2012 to 2013"
     end
 
     it "calculates the overall cost" do
-      fill_in "total-annual-income-before-tax", :with => "5000"
+      fill_in "total_annual_income", :with => "5000"
       fill_in "children", :with => "2"
       click_button "Go"
-      within ".calculator-results" do
-        page.should have_content("You owe £1,000")
+      within ".outcome" do
+        page.should have_content("£1,000")
       end
     end
   end
