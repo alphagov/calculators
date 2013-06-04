99766a39744c1ce88b08641ea42013b282c3e5b3
Jack Franklin <jack@jackfranklin.net>
2013-06-04 11:00:00 +0100

    Move main form to /main and leave landing page alone
    Store all params cross request

--- app/controllers/child_benefit_tax_controller.rb
+++ app/controllers/child_benefit_tax_controller.rb
@@ -1,11 +1,10 @@
 class ChildBenefitTaxController < ApplicationController
   def landing
+  end
+
+  def main
     @calculator = ChildBenefitTaxCalculator.new(params)
     @show_new_child_form = params[:commit] == "Add a new starting child"
     @show_old_child_form = params[:commit] == "Add a new stopping child"
-
-    if params[:commit] == "Go"
-      @calculator = ChildBenefitTaxCalculator.new(params)
-    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Calculators::Application.routes.draw do
   get "/child-benefit-tax-calculator" => "child_benefit_tax#landing"
-  post "/child-benefit-tax-calculator" => "child_benefit_tax#landing"
+  get "/child-benefit-tax-calculator/main" => "child_benefit_tax#main"
+  post "/child-benefit-tax-calculator/main" => "child_benefit_tax#main"
 end
