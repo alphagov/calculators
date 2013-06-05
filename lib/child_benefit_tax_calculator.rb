144775c27a54107dc009770492639231262bb329
Jack Franklin <jack@jackfranklin.net>
2013-06-05 11:00:00 +0100

    Fix the adjusted_net_income calculation
    Add in some of the calculations for calculating the owed amount

--- lib/child_benefit_tax_calculator.rb
+++ lib/child_benefit_tax_calculator.rb
@@ -1,25 +1,80 @@
 class ChildBenefitTaxCalculator
-  attr_accessor :total_annual_income, :children_count, :starting_children, :stopping_children, :tax_year
+  attr_reader :adjusted_net_income, :children_count, :starting_children, :stopping_children, :tax_year
 
   def initialize(params)
     @total_annual_income = params[:total_annual_income].to_i
-    gross_pension_contributions = params[:gross_pension_contributions].to_i
-    net_pension_contributions = params[:net_pension_contributions].to_i
-    trading_losses_self_employed = params[:trading_losses_self_employed].to_i
-    gift_aid_donations = params[:gift_aid_donations].to_i
+    @gross_pension_contributions = params[:gross_pension_contributions].to_i
+    @net_pension_contributions = params[:net_pension_contributions].to_i
+    @trading_losses_self_employed = params[:trading_losses_self_employed].to_i
+    @gift_aid_donations = params[:gift_aid_donations].to_i
+
+    @adjusted_net_income = calculate_adjusted_income(params[:adjusted_net_income].to_i)
+
     @starting_children = params[:starting_children] || []
     @stopping_children = params[:stopping_children] || []
     @tax_year = params[:year].to_i
-
-    if @total_annual_income == 0
-      @total_annual_income = gross_pension_contributions + net_pension_contributions + trading_losses_self_employed + gift_aid_donations
-    end
-
     @children_count = params[:children_count].to_i
   end
 
   def owed
-    @total_annual_income * 0.1 * @children_count
+    benefit_values = if @starting_children.empty? && @stopping_children.empty?
+      benefits_no_starting_stopping_children
+    else
+      benefits_with_changing_children
+    end
+
+    benefit_values[:benefit_tax] = percent_tax_charge * benefit_values[:benefit_taxable_amount]
+  end
+
+
+
+  private
+
+
+  def percent_tax_charge
+    20
+  end
+
+  def benefits_no_starting_stopping_children
+
+    child_benefit_start_date = @tax_year == "2012" ? Date.parse("2012-04-06") : Date.parse("2013-04-06")
+    child_benefit_end_date = @tax_year == "2012" ? Date.parse("2013-04-05") : Date.parse("2014-04-05")
+
+    # amount they would claim over a year
+    benefit_claimed_weeks = ((child_benefit_end_date - child_benefit_start_date) / 52).floor
+    benefit_taxable_weeks = ((child_benefit_end_date - Date.parse("2013-01-07")) / 52).floor
+
+    # benefit rates fixed until April 2014: gov.uk/child-benefit-rates
+    # 20.30 for 1st child, 13.40 for each next child
+    benefit_claimed_amount = 0
+    @children_count.times do |child_number|
+      benefit_claimed_amount += ( child_number == 0 ? 20.3 : 13.4 )
+    end
+
+    {
+      :benefit_taxable_amount => (benefit_claimed_amount / 52) * benefit_taxable_weeks,
+      :benefit_taxable_weeks => benefit_taxable_weeks,
+      :benefit_claimed_amount => benefit_claimed_amount,
+      :benefit_claimed_weeks => benefit_claimed_weeks,
+    }
+  end
+
+  def benefits_with_changing_children
+    {
+      :benefit_taxable_amount => 0,
+      :benefit_taxable_weeks => 0,
+      :benefit_claimed_amount => 0,
+      :benefit_claimed_weeks => 0,
+    }
+
+  end
+
+  def calculate_adjusted_income(adjusted_income)
+    if adjusted_income == 0
+      @total_annual_income - @gross_pension_contributions - (@net_pension_contributions * 1.25) - @trading_losses_self_employed - (@gift_aid_donations * 1.25)
+    else
+      adjusted_income
+    end
   end
 
 end
--- spec/lib/child_benefit_tax_calculator_spec.rb
+++ spec/lib/child_benefit_tax_calculator_spec.rb
@@ -1,34 +1,33 @@
 require 'spec_helper'
 
 describe ChildBenefitTaxCalculator do
-  it "calculates the proper owed value" do
+  it "uses the adjusted net income if it's passed in" do
     calc = ChildBenefitTaxCalculator.new({
-      :total_annual_income => 20,
-      :children_count => 1
+      :adjusted_net_income=> 20
     })
-    calc.owed.should == 2
+    calc.adjusted_net_income.should == 20
   end
 
-  it "calculates the total amount from other values" do
+  it "calculates net income from other values" do
     calc = ChildBenefitTaxCalculator.new({
       :gross_pension_contributions => 10,
       :net_pension_contributions => 10,
       :trading_losses_self_employed => 10,
       :gift_aid_donations => 10,
-      :children_count => 1
+      :total_annual_income => 100,
     })
-    calc.total_annual_income.should == 40
-    calc.owed.should == 4
+    calc.adjusted_net_income.should == 55.0
   end
 
   it "uses the total annual income if both are supplied" do
     calc = ChildBenefitTaxCalculator.new({
-      :total_annual_income => 20,
       :gross_pension_contributions => 10,
       :net_pension_contributions => 10,
       :trading_losses_self_employed => 10,
-      :children_count => 1
+      :gift_aid_donations => 10,
+      :total_annual_income => 100,
+      :adjusted_net_income => 20
     })
-    calc.total_annual_income.should == 20
+    calc.adjusted_net_income.should == 20
   end
 end
