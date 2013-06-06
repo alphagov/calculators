02a776a6cb89c643dc1696dea1be77ea3b101e89
Jack Franklin <jack@jackfranklin.net>
2013-06-06 10:41:35 +0100

    Tidying up and refactoring of the calculator class

--- lib/child_benefit_tax_calculator.rb
+++ lib/child_benefit_tax_calculator.rb
@@ -1,15 +1,21 @@
 class ChildBenefitTaxCalculator
   attr_reader :adjusted_net_income, :children_count, :starting_children, :stopping_children, :tax_year
 
+  FIRST_CHILD_RATE = 20.3
+  FURTHER_CHILD_RATE = 13.4
+
+  TAX_YEARS = {
+    "2012" => [Date.parse("2012-04-06"), Date.parse("2013-04-05")],
+    "2013" => [Date.parse("2013-04-06"), Date.parse("2014-04-05")]
+  }
+
   def initialize(params)
     @total_annual_income = params[:total_annual_income].to_i
     @gross_pension_contributions = params[:gross_pension_contributions].to_i
     @net_pension_contributions = params[:net_pension_contributions].to_i
     @trading_losses_self_employed = params[:trading_losses_self_employed].to_i
     @gift_aid_donations = params[:gift_aid_donations].to_i
-
     @adjusted_net_income = calculate_adjusted_income(params[:adjusted_net_income].to_i)
-
     @starting_children = params[:starting_children] || []
     @stopping_children = params[:stopping_children] || []
     @tax_year = params[:year].to_i
@@ -17,7 +23,7 @@ class ChildBenefitTaxCalculator
   end
 
   def owed
-    it_values = if @starting_children.empty? && @stopping_children.empty?
+    if @starting_children.empty? && @stopping_children.empty?
       benefits_no_starting_stopping_children
     else
       benefits_with_changing_children
@@ -30,14 +36,13 @@ class ChildBenefitTaxCalculator
 
   private
 
-
   def benefits_no_starting_stopping_children
     # benefit rates fixed until April 2014: gov.uk/child-benefit-rates
     # 20.30 for 1st child, 13.40 for each next child
     benefit_weekly_amount = 0
 
     @children_count.times do |child_number|
-      benefit_weekly_amount += ( child_number == 0 ? 20.3 : 13.4 )
+      benefit_weekly_amount += ( child_number == 0 ? FIRST_CHILD_RATE : FURTHER_CHILD_RATE )
     end
 
     benefit_claimed_amount = benefit_weekly_amount * 52
@@ -76,11 +81,10 @@ class ChildBenefitTaxCalculator
       sum + weekly_sum_for_children(n)
     end
 
-
     if @tax_year == 2012
       # only taxable from 7/01/2013
       taxed_weeks_children = all_weeks_children.select do |key, val|
-        (Date.parse("2013-01-07")..child_benefit_end_date).include?(key)
+        (Date.parse("2013-01-07")..child_benefit_end_date).cover?(key)
       end
       taxed_weeks_sum = taxed_weeks_children.values.inject(0) do |sum, n|
         sum + weekly_sum_for_children(n)
@@ -102,7 +106,7 @@ class ChildBenefitTaxCalculator
 
   def weekly_sum_for_children(num_children)
     if num_children > 0
-      ( 1 * 20.3 + (num_children - 1 ) * 13.4 )
+      1 * FIRST_CHILD_RATE + (num_children - 1 ) * FURTHER_CHILD_RATE
     else
       0
     end
@@ -110,6 +114,7 @@ class ChildBenefitTaxCalculator
 
   def taxable_weeks
     if @tax_year == 2012
+      # special case for 2012-13, only weeks from 7th Jan 2013 are taxable
       benefit_taxable_weeks(Date.parse("2013-01-07"), child_benefit_end_date)
     else
       benefit_taxable_weeks(child_benefit_start_date, child_benefit_end_date)
@@ -119,10 +124,10 @@ class ChildBenefitTaxCalculator
   def percent_tax_charge
     if @adjusted_net_income >= 60001
       100
-    elsif @adjusted_net_income >= 59900 && @adjusted_net_income <= 60000
+    elsif (59900..60000).cover?(@adjusted_net_income)
       99
     else
-      ((@adjusted_net_income - 50000)/100).floor
+      ((@adjusted_net_income - 50000)/100.0).floor
     end
   end
 
@@ -131,15 +136,15 @@ class ChildBenefitTaxCalculator
   end
 
   def parse_child_date(date)
-    Date.parse("#{date[:year]}-#{date[:month]}-#{date[:day]}")
+    Date.new(date[:year], date[:month], date[:day])
   end
 
   def child_benefit_start_date
-    @tax_year == 2012 ? Date.parse("2012-04-06") : Date.parse("2013-04-06")
+    TAX_YEARS[@tax_year.to_s].first
   end
 
   def child_benefit_end_date
-    @tax_year == 2012 ? Date.parse("2013-04-05") : Date.parse("2014-04-05")
+    TAX_YEARS[@tax_year.to_s].last
   end
 
   def calculate_adjusted_income(adjusted_income)
