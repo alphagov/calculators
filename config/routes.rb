54a07541799440ed6277fc22f3837dae33a74c46
Jack Franklin <jack@jackfranklin.net>
2013-06-06 15:23:07 +0100

    Add format => false back onto routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Calculators::Application.routes.draw do
-  get "/child-benefit-tax-calculator" => "child_benefit_tax#landing"
-  get "/child-benefit-tax-calculator/main" => "child_benefit_tax#main"
+  get "/child-benefit-tax-calculator" => "child_benefit_tax#landing", :format => false
+  get "/child-benefit-tax-calculator/main" => "child_benefit_tax#main", :format => false
   post "/child-benefit-tax-calculator/main" => "child_benefit_tax#main"
 end
