e3063b3c1eb799c9b3f59cb98b53187098b4ea6d
Jack Franklin <jack@jackfranklin.net>
2013-06-11 10:28:19 +0100

    add some comments for clarity

--- app/controllers/child_benefit_tax_controller.rb
+++ app/controllers/child_benefit_tax_controller.rb
@@ -10,14 +10,19 @@ class ChildBenefitTaxController < ApplicationController
     @calculator = ChildBenefitTaxCalculator.new(params)
     @show_new_child_form = params[:commit] == "Add another child"
     @show_old_child_form = params[:commit] == "Add a new stopping child"
+
     @show_extra_income = (params[:commit] == "I don't know my net income" || params[:show_extra_income] == "true")
 
+    # for adding a new starting child
     @start_child_show_starting_date = params[:does_have_starting_children] && params[:does_have_starting_children] == "Yes"
 
+    # do we have a new starting child & need to show the field for their stopping date?
     @start_child_show_stopping_date = params[:starting_child_does_stop] && params[:starting_child_does_stop] == "Yes"
 
+    # does the new start child not have a stopping date?
     @start_child_no_stopping_date = params[:starting_child_does_stop] && params[:starting_child_does_stop] == "No"
 
+    # show the stopping date for a stopping child
     @stop_child_show_date = params[:does_have_stopping_children] && params[:does_have_stopping_children] == "Yes"
   end
 end
