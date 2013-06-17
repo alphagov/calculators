c6a1d8a61757f21698ac452634bd1ca9d73392fe
Jack Franklin <jack@jackfranklin.net>
2013-06-17 10:13:54 +0100

    Make the form get

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Calculators::Application.routes.draw do
   get "/child-benefit-tax-calculator" => "child_benefit_tax#landing", :format => false
   get "/child-benefit-tax-calculator/main" => "child_benefit_tax#main", :format => false
-  post "/child-benefit-tax-calculator/main" => "child_benefit_tax#main"
 end
