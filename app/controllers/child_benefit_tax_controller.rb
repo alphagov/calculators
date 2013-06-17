4edd3875546c18f272da774e94aa8c3ad7944b4f
Jack Franklin <jack@jackfranklin.net>
2013-06-17 11:23:50 +0100

    Allow the user to be jumped to a specific anchor
    This means there's not that annoying scrolling when you're half way down
    the form and click on a button. Done by doing some processing of the URL
    parameters in a process_form method, which then redirects through to the
    main method with the appropriate #foo hash for taking the user to a
    point, depending on which button was used to submit the form.

--- app/controllers/child_benefit_tax_controller.rb
+++ app/controllers/child_benefit_tax_controller.rb
@@ -2,10 +2,34 @@ class ChildBenefitTaxController < ApplicationController
   def landing
   end
 
-  def main
-    unless params[:year]
-      redirect_to :action => :landing
+  def process_form
+    redirect_if_no_year
+
+    anchor = ""
+    if params[:add_another_starting_child_submit].present?
+      anchor = "add_new_starting_child"
+    elsif params[:commit] == "I don't know my net income"
+      anchor = "adjusted_income"
+    elsif params[:add_another_stopping_child_submit].present?
+      anchor = "add_new_stopping_child"
+    elsif params[:commit] == "Estimate your tax charge"
+      anchor = "results_box"
     end
+
+    redirect_obj = { :action => :main, :anchor => anchor }
+
+    # params from the form
+    # that need to be passed to the main method, through the redirect
+    [:total_annual_income, :gross_pension_contributions, :net_pension_contributions, :trading_losses_self_employed, :gift_aid_donations, :adjusted_net_income, :starting_children, :stopping_children, :year, :children_count, :commit, :add_another_starting_child_submit, :add_another_stopping_child_submit, :does_have_starting_children, :starting_child_does_stop, :does_have_stopping_children].each do |name|
+      redirect_obj[name] = params[name]
+    end
+
+    redirect_to(redirect_obj)
+  end
+
+
+  def main
+    redirect_if_no_year
 
     @calculator = ChildBenefitTaxCalculator.new(params)
     @show_new_child_form = params[:add_another_starting_child_submit] == "Add another child"
@@ -13,6 +37,7 @@ class ChildBenefitTaxController < ApplicationController
 
     @show_extra_income = (params[:commit] == "I don't know my net income" || params[:show_extra_income] == "true")
 
+    # TODO: do these vars get used?
     # for adding a new starting child
     @start_child_show_starting_date = params[:does_have_starting_children] && params[:does_have_starting_children] == "Yes"
 
@@ -24,5 +49,12 @@ class ChildBenefitTaxController < ApplicationController
 
     # show the stopping date for a stopping child
     @stop_child_show_date = params[:does_have_stopping_children] && params[:does_have_stopping_children] == "Yes"
+  end
+
+  private
+  def redirect_if_no_year
+    unless params[:year]
+      redirect_to :action => :landing
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Calculators::Application.routes.draw do
   get "/child-benefit-tax-calculator" => "child_benefit_tax#landing", :format => false
   get "/child-benefit-tax-calculator/main" => "child_benefit_tax#main", :format => false
+  get "/child-benefit-tax-calculator/process_form" => "child_benefit_tax#process_form", :format => false
 end
