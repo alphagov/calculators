0d2984c6bcb78b9a2179714c2bc84947cd70010b
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2013-07-22 14:34:40 +0000

    Clean up controller params handling

--- app/controllers/child_benefit_tax_controller.rb
+++ app/controllers/child_benefit_tax_controller.rb
@@ -4,12 +4,8 @@ class ChildBenefitTaxController < ApplicationController
   end
 
   def process_form
-    anchor = if params[:add_another_starting_child_submit].present?
-      "add_new_starting_child"
-    elsif params[:commit] == "I don't know my adjusted net income"
+    anchor = if params[:commit] == "I don't know my adjusted net income"
       "adjusted_income"
-    elsif params[:add_another_stopping_child_submit].present?
-      "add_new_stopping_child"
     elsif params[:commit] == "Get your estimate"
       "results_box"
     else
@@ -20,7 +16,9 @@ class ChildBenefitTaxController < ApplicationController
 
     # params from the form
     # that need to be passed to the main method, through the redirect
-    [:total_annual_income, :gross_pension_contributions, :net_pension_contributions, :trading_losses_self_employed, :gift_aid_donations, :adjusted_net_income, :starting_children, :stopping_children, :year, :children_count, :commit, :add_another_starting_child_submit, :add_another_stopping_child_submit, :does_have_starting_children, :starting_child_does_stop, :does_have_stopping_children].each do |name|
+    [:total_annual_income, :gross_pension_contributions, :net_pension_contributions,
+     :trading_losses_self_employed, :gift_aid_donations, :adjusted_net_income,
+     :starting_children, :year, :commit].each do |name|
       redirect_obj[name] = params[name]
     end
 
