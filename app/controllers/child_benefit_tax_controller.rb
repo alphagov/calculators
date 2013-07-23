976bdd2c5dfcdd2e854b5f15e0688c227e286746
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2013-07-23 14:33:06 +0000

    Rename constant

--- app/controllers/child_benefit_tax_controller.rb
+++ app/controllers/child_benefit_tax_controller.rb
@@ -1,6 +1,6 @@
 class ChildBenefitTaxController < ApplicationController
 
-  FORM_PARAM_KEYS = [:total_annual_income, :gross_pension_contributions, :net_pension_contributions,
+  CALC_PARAM_KEYS = [:total_annual_income, :gross_pension_contributions, :net_pension_contributions,
      :trading_losses_self_employed, :gift_aid_donations, :adjusted_net_income,
      :children_count, :starting_children, :year]
 
@@ -15,13 +15,12 @@ class ChildBenefitTaxController < ApplicationController
       redirect_hash.merge!(:anchor => anchor.to_s) if params[anchor]
     end
 
-    FORM_PARAM_KEYS.each do |name|
+    CALC_PARAM_KEYS.each do |name|
       redirect_hash[name] = params[name]
     end
 
     redirect_to(redirect_hash)
   end
-
 
   def main
     @calculator = ChildBenefitTaxCalculator.new(params)
