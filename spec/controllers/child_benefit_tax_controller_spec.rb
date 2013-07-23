b80473c4719d245f56554a882100ad1809f6d76f
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2013-07-23 13:20:22 +0000

    Tidy up anchor logic

--- spec/controllers/child_benefit_tax_controller_spec.rb
+++ spec/controllers/child_benefit_tax_controller_spec.rb
@@ -19,19 +19,19 @@ describe ChildBenefitTaxController do
 
   describe "GET process_form" do
     it "should place a 'starting_children' anchor onto the redirected response" do
-      route_params = { :commit => "Update" }
+      route_params = { :children => "Update" }
       get 'process_form', route_params
-      response.should redirect_to(:action => :main, :params => route_params, :anchor => "starting_children")
+      response.should redirect_to(:action => :main, :anchor => "children")
     end
     it "should place an 'adjusted_income' anchor onto the redirected response" do
-      route_params = { :commit => "I don't know my adjusted net income" }
+      route_params = { :adjusted_income => "I don't know my adjusted net income" }
       get 'process_form', route_params
-      response.should redirect_to(:action => :main, :params => route_params, :anchor => "adjusted_income")
+      response.should redirect_to(:action => :main, :anchor => "adjusted_income")
     end
-    it "should place an 'results_box' anchor onto the redirected response" do
-      route_params = { :commit => "Get your estimate" }
+    it "should place an 'results' anchor onto the redirected response" do
+      route_params = { :results => "Get your estimate" }
       get 'process_form', route_params
-      response.should redirect_to(:action => :main, :params => route_params, :anchor => "results_box")
+      response.should redirect_to(:action => :main, :anchor => "results")
     end
   end
 
