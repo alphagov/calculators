442eea75bfb3828c4c95e270562eea4f1d605416
Amy Whitney <amy.whitney@digital.cabinet-office.gov.uk>
2013-07-25 10:39:51 +0100

    update button for adding children, javascript functionality and updated styling

--- app/assets/javascripts/child-benefit-tax-calculator.js
+++ app/assets/javascripts/child-benefit-tax-calculator.js
@@ -1,5 +1,7 @@
 GOVUK = GOVUK || {};
 GOVUK.ChildBenefitTaxCalculator = (function(){
   $("#children_count").on('change', function(e){
+console.log("foo")
+  	$("#child_benefit_tax_calculator").submit();
   });
 })();
