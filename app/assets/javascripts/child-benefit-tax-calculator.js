aeb7224453cc607180d025f437ebc5a6e2e49123
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2013-07-25 15:20:05 +0000

    Remove console debug statement

--- app/assets/javascripts/child-benefit-tax-calculator.js
+++ app/assets/javascripts/child-benefit-tax-calculator.js
@@ -1,7 +1,6 @@
 GOVUK = GOVUK || {};
 GOVUK.ChildBenefitTaxCalculator = (function(){
   $("#children_count").on('change', function(e){
-console.log("foo")
-  	$("#child_benefit_tax_calculator").submit();
+    $("#child_benefit_tax_calculator").submit();
   });
 })();
