74e417e8a45447ccc548d796fa6f55a1f68ec484
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2013-07-29 14:14:35 +0000

    Add adjusted net income calculator

---
+++ app/models/adjusted_net_income_calculator.rb
@@ -0,0 +1,32 @@
+class AdjustedNetIncomeCalculator
+  
+  PARAM_KEYS = [:gross_income, :other_income, :pension_contributions_from_pay,
+    :retirement_annuities, :cycle_scheme, :childcare, :pensions, :property,
+    :non_employment_income, :gift_aid_donations, :outgoing_pension_contributions]
+
+  def initialize(params)
+    PARAM_KEYS.each do |key|
+      instance_variable_set :"@#{key}", integer_value(params[key])
+    end
+  end
+
+  def adjusted_net_income
+    (@gross_income + @other_income + @pensions + @property + @non_employment_income) - deductions
+  end
+
+  private
+
+  def deductions
+    grossed_up(@pension_contributions_from_pay) + grossed_up(@gift_aid_donations) + 
+      @retirement_annuities + @cycle_scheme + @childcare
+  end
+
+  def grossed_up(amount)
+    (amount * 1.25)
+  end
+
+  def integer_value(str)
+    str.gsub!(/\D/,'') if str.is_a?(String)
+    str.to_i
+  end
+end
---
+++ spec/models/adjusted_net_income_calculator_spec.rb
@@ -0,0 +1,23 @@
+# encoding: UTF-8
+require 'spec_helper'
+
+describe AdjustedNetIncomeCalculator do
+
+  describe "adjusted_net_income" do
+    it "should calculate with minimal parameters" do
+      AdjustedNetIncomeCalculator.new(:gross_income => "£60,000").adjusted_net_income.should == 60000
+    end
+    it "should ignore other params" do
+      AdjustedNetIncomeCalculator.new(:foo => "bar").adjusted_net_income.should == 0
+    end
+    it "should calculate parsed parameters" do
+      AdjustedNetIncomeCalculator.new(
+        :gross_income => "£60,000", :other_income => "£5,000", :pensions => "£1000",
+        :property => "£2,000", :non_employment_income => "£2000",
+        :pension_contributions_from_pay => "£1000", :retirement_annuities => "£1000",
+        :cycle_scheme => "£1,000", :childcare => "£2,000"
+      ).adjusted_net_income.should == 64750
+    end
+  end
+
+end 
