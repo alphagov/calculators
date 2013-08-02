4073080492958c784764b77488e9751dffbeeae3
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2013-08-02 12:11:47 +0000

    Add gift aid to ANI test

--- spec/models/adjusted_net_income_calculator_spec.rb
+++ spec/models/adjusted_net_income_calculator_spec.rb
@@ -14,7 +14,7 @@ describe AdjustedNetIncomeCalculator do
       calc = AdjustedNetIncomeCalculator.new(
         :gross_income => "£60,000", :other_income => "£5,000", :pensions => "£1000",
         :property => "£2,000", :non_employment_income => "£2000",
-        :pension_contributions_from_pay => "£1000", :retirement_annuities => "£1000",
+        :pension_contributions_from_pay => "£1000", :gift_aid_donations => "£500", :retirement_annuities => "£1000",
         :cycle_scheme => "£1,000", :childcare => "£2,000", :outgoing_pension_contributions => "£2 000"
       )
       calc.gross_income.should == 60000
@@ -22,12 +22,15 @@ describe AdjustedNetIncomeCalculator do
       calc.pensions.should == 1000
       calc.property.should == 2000
       calc.non_employment_income.should == 2000
+
       calc.pension_contributions_from_pay.should == 1000
+      calc.gift_aid_donations.should == 500
       calc.retirement_annuities.should == 1000
       calc.cycle_scheme.should == 1000
       calc.childcare.should == 2000
       calc.outgoing_pension_contributions.should == 2000
-      calc.calculate_adjusted_net_income.should == 62750
+
+      calc.calculate_adjusted_net_income.should == 62125
     end
   end
 
