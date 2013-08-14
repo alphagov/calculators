a1072e436cb3cc8d99c17ad001384e50a35c0c89
Jordan Hatch <jordan@jordanh.net>
2013-08-14 15:14:47 +0000

    Correct matcher to search for h3 rather than h2

--- spec/support/child_benefit.rb
+++ spec/support/child_benefit.rb
@@ -1,7 +1,7 @@
 RSpec::Matchers::define :contain_child_benefit_value do |text|
   match do |page|
     within ".results" do
-      within :xpath, ".//div[contains(@class, 'results_estimate')][.//h2[.='Child Benefit received']]" do
+      within :xpath, ".//div[contains(@class, 'results_estimate')][.//h3[.='Child Benefit received']]" do
         page.should have_content(text)
       end
     end
