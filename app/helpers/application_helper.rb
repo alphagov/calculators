062bc001d43d3503a92f4db5c8e1b56c26df6be2
Jordan Hatch <jordan@jordanh.net>
2013-08-16 16:05:41 +0100

    Allow an edition number to be specified for internal links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,8 +9,8 @@ module ApplicationHelper
     "<h2><span class='steps' id='step-#{num}'><span class='visuallyhidden'>Step #{num}</span></span>#{text}</h2>".html_safe
   end
 
-  def internal_url(path)
-    PRIVATE_FRONTEND_INTERNAL_LINKS ? "#{Plek.current.find('private-frontend')}#{path}?edition=1" : "#{path}"
+  def internal_url(path, edition=1)
+    PRIVATE_FRONTEND_INTERNAL_LINKS ? "#{Plek.current.find('private-frontend')}#{path}?edition=#{edition}" : "#{path}"
   end
 
 end
