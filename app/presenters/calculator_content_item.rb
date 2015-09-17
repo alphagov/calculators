e2bb09076b059a40bed39d9843b86a070ac0c629
tijmen <tijmen@gmail.com>
2015-09-17 14:10:50 +0100

    Add presenter for calculator item
    CalculatorContentItem produces a valid representation for the
    content-store.

---
+++ app/presenters/calculator_content_item.rb
@@ -0,0 +1,27 @@
+# Renders a calculator for the publishing-api.
+class CalculatorContentItem
+  attr_reader :calculator
+
+  def initialize(calculator)
+    @calculator = calculator
+  end
+
+  def base_path
+    '/' + calculator.slug
+  end
+
+  def payload
+    {
+      title: calculator.title,
+      format: 'placeholder_calculator',
+      publishing_app: 'calculators',
+      rendering_app: 'calculators',
+      update_type: 'minor',
+      locale: 'en',
+      public_updated_at: Time.now.iso8601,
+      routes: [
+        { type: 'exact', path: base_path }
+      ]
+    }
+  end
+end
---
+++ spec/presenters/calculator_content_item_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe CalculatorContentItem do
+  describe '#payload' do
+    it 'is valid against the schema' do
+      calculator = Calculator.all.first
+
+      payload = CalculatorContentItem.new(calculator).payload
+
+      expect(payload).to be_valid_against_schema('placeholder')
+    end
+
+    it 'has the correct data' do
+      calculator = Calculator.all.first
+
+      payload = CalculatorContentItem.new(calculator).payload
+
+      expect(payload[:title]).to eql('Child Benefit tax calculator')
+    end
+  end
+
+  describe '#base_path' do
+    it 'has the correct base path' do
+      calculator = Calculator.all.first
+
+      base_path = CalculatorContentItem.new(calculator).base_path
+
+      expect(base_path).to eql("/child-benefit-tax-calculator")
+    end
+  end
+end
