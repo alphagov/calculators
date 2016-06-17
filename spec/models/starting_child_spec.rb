74abec573f247941f5da90f29685517bd2e81646
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2016-06-17 14:35:54 +0100

    Refactor how the next Monday is calculated
    The next monday is calculated in two places. Before the changes in
    commit 994d1db, these calculations produced different result:
    
    1. The adjusted_start_date attribute on the child is set only once and was
    set to the Monday after the start date. This has since been changed to
    also include the start date, if the start date is a Monday.
    
    2. The code that determines how many weeks to calculate child benefit for
    counts the number of Mondays in the tax year, including the first day of the
    tax year if that happened to be a Monday.
    
    Now both return the same date, so the next Monday calculation function (monday_on_or_after)
    is being used in both instances.
    
    The adjusted start date on the Starting Child is a property that changes
    the child's start date to the next valid monday to comply with the rules.
    
    Models shouldn't be aware of the rules of a calculator so this property has been removed.
    The next Monday calculation function is instead being called  on the Starting Child
    start date to extrapolate what the benefit start date would be.
    
    The tests that previously tested the adjusted start date property have been removed and new
    tests have been added to the child benefit tax calculator model.
    
    monday_on_or_after uses the Rails DateAndTime helper functions to calculate the next Monday
    
    See API documentation:
    http://api.rubyonrails.org/classes/DateAndTime/Calculations.html#method-i-next_week

--- spec/models/starting_child_spec.rb
+++ spec/models/starting_child_spec.rb
@@ -45,29 +45,4 @@ describe StartingChild, type: :model do
     )
     expect(child).to be_valid
   end
-
-  describe "adjusted_start_date" do
-    it "should not adjust the start date if start date is a Monday" do
-      child = StartingChild.new(start: { year: "2013", month: "01", day: "07" })
-      expect(child.adjusted_start_date).to eq(Date.parse("07 January 2013"))
-    end
-
-    it "should return the next Monday for the provided start date" do
-      child = StartingChild.new(start: { year: "2012", month: "01", day: "01" })
-      expect(child.adjusted_start_date).to eq(Date.parse("2 January 2012"))
-
-      child = StartingChild.new(start: { year: "2013", month: "05", day: "08" })
-      expect(child.adjusted_start_date).to eq(Date.parse("13 May 2013"))
-
-      child = StartingChild.new(start: { year: "2013", month: "08", day: "13" })
-      expect(child.adjusted_start_date).to eq(Date.parse("19 August 2013"))
-
-      child = StartingChild.new(start: { year: "2013", month: "01", day: "06" })
-      expect(child.adjusted_start_date).to eq(Date.parse("7 January 2013"))
-    end
-
-    it "should not blow up with a nil start date" do
-      expect(StartingChild.new(start: {}).adjusted_start_date).to be_nil
-    end
-  end
 end
