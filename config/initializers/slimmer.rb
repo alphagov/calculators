8d14499819dd0f79fb843e41a7fe3f03b066f15e
Tijmen Brommet <tijmen@gmail.com>
2017-02-22 17:18:01 +0000

    Bump slimmer to use Rails' cache
    This bumps slimmer to make it use the rails cache and send a user agent:
    
    - https://github.com/alphagov/slimmer/pull/184
    - https://github.com/alphagov/slimmer/pull/183
    
    It will prevent this app from making many needless requests to `static`.
    
    https://trello.com/c/D9HmkJwI

--- config/initializers/slimmer.rb
+++ config/initializers/slimmer.rb
@@ -1,8 +1,6 @@
 Calculators::Application.configure do
   config.slimmer.logger = Rails.logger
 
-  config.slimmer.use_cache = true if Rails.env.production?
-
   if Rails.env.development?
     config.slimmer.asset_host = ENV["STATIC_DEV"] || "http://static.dev.gov.uk"
   end
