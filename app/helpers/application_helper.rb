ba702ea5a838920d2f3416be02f3f4b4029c6f4b
Paul Hayes <fofr@users.noreply.github.com>
2017-05-25 14:56:14 +0100

    Use govspeak component styles for content
    * Port styles to calculators for lists and paragraphs in form
    * Send results to govspeak component
    * Override govspeak styles for h3s and big numbers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,8 +4,7 @@ module ApplicationHelper
     File.mtime(Rails.root.join('REVISION')).to_date rescue Date.today
   end
 
-  def step(num, text, description = nil)
-    text << "<span id='step-#{num}-description'>#{description}</span>" if description.present?
-    "<div class=\"govuk-govspeak\"><ul class=\"steps\"><li class=\"steps-step#{num}\"><h2>#{text}</h2></li></ul></div>".html_safe
+  def step(num, text)
+    "<h2 class=\"step step-#{num}\">#{text}</h2>".html_safe
   end
 end
