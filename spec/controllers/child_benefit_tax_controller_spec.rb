27eb34738a218ed04b9226e91d58de5c210c0715
Ikenna N. Okpala <me@ikennaokpala.com>
2017-06-22 12:18:10 +0100

    Fix test sytle deprecation warnings
    This updates the controller tests to fix the deprecation warning about
    the "New keyword style:"

--- spec/controllers/child_benefit_tax_controller_spec.rb
+++ spec/controllers/child_benefit_tax_controller_spec.rb
@@ -15,13 +15,13 @@ describe ChildBenefitTaxController, type: :controller do
 
     describe "GET main" do
       it "should create a calculator using params" do
-        get 'main', year: '2013'
+        get :main, params: { year: '2013' }
         expect(response).to be_success
         expect(assigns(:calculator).tax_year).to eq(2013)
         expect(assigns(:adjusted_net_income_calculator).calculate_adjusted_net_income).to eq(0)
       end
       it "should run calculator validations" do
-        get 'main', results: "Get your estimate"
+        get :main, params: { results: "Get your estimate" }
         expect(response).to be_success
         expect(assigns(:calculator).errors.has_key?(:tax_year)).to eq(true)
       end
@@ -29,18 +29,18 @@ describe ChildBenefitTaxController, type: :controller do
 
     describe "GET process_form" do
       it "should place a 'starting_children' anchor onto the redirected response" do
-        route_params = { children: "Update" }
-        get "process_form", route_params
+        route_params = { params: { children: "Update" } }
+        get :process_form, route_params
         expect(response).to redirect_to(action: :main, anchor: "children")
       end
       it "should place an 'adjusted_income' anchor onto the redirected response" do
-        route_params = { adjusted_income: "I don't know my adjusted net income" }
-        get "process_form", route_params
+        route_params = { params: { adjusted_income: "I don't know my adjusted net income" } }
+        get :process_form, route_params
         expect(response).to redirect_to(action: :main, anchor: "adjusted_income")
       end
       it "should place an 'results' anchor onto the redirected response" do
         route_params = { results: "Get your estimate" }
-        get "process_form", route_params
+        get :process_form, params: route_params
         expect(response).to redirect_to(action: :main, params: route_params, anchor: "results")
       end
     end
