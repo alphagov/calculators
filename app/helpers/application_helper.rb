8f776b369fd3149b38f905282173e4510f59229e
Murray Steele <muz@h-lame.com>
2017-08-23 09:27:45 +0000

    Add current_path to the survey link in the beta banner
    This lets the survey team know where each response has come from and
    allows them to filter by section of the site.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
   def step(num, text)
     "<h2 class=\"step step-#{num}\">#{text}</h2>".html_safe
   end
+
+  def current_path_without_query_string
+    request.original_fullpath.split("?", 2).first
+  end
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -6,4 +6,16 @@ describe ApplicationHelper, type: :helper do
       expect(helper.step(1, "Blah")).to eq("<h2 class=\"step step-1\">Blah</h2>")
     end
   end
+
+  describe '#current_path_without_query_string' do
+    it "returns the path of the current request" do
+      allow(helper).to receive(:request).and_return(ActionDispatch::TestRequest.new("PATH_INFO" => '/foo/bar'))
+      assert_equal '/foo/bar', helper.current_path_without_query_string
+    end
+
+    it "returns the path of the current request stripping off any query string parameters" do
+      allow(helper).to receive(:request).and_return(ActionDispatch::TestRequest.new("PATH_INFO" => '/foo/bar', "QUERY_STRING" => 'ham=jam&spam=gram'))
+      assert_equal '/foo/bar', helper.current_path_without_query_string
+    end
+  end
 end
