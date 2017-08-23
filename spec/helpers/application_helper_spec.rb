b919e3b3a713026048f1534e85e00a6322eff416
Murray Steele <muz@h-lame.com>
2017-08-23 09:26:38 +0000

    Introduce context for each helper method in application_helper_spec
    This keeps the test tidier.  We also make use of the `helper` object when
    invoking methods under test as that's the preferred rspec way in tests
    for view helpers.

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,7 +1,9 @@
 require "spec_helper"
 
 describe ApplicationHelper, type: :helper do
-  it "generates the html for a step" do
-    expect(step(1, "Blah")).to eq("<h2 class=\"step step-1\">Blah</h2>")
+  describe '#step' do
+    it "generates the html for a step" do
+      expect(helper.step(1, "Blah")).to eq("<h2 class=\"step step-1\">Blah</h2>")
+    end
   end
 end
