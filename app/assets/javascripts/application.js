55943e741ac4d85cfac1e07d6af70cc55b52cc7e
Tijmen Brommet <tijmen@gmail.com>
2018-02-21 12:40:37 +0000

    Use the feedback component
    This replaces the "Is there anything wrong?" link with the new feedback
    component. This component asks the user if the page is useful and
    reports the results to Google Analytics.
    
    See https://github.com/alphagov/govuk_publishing_components/pull/163
    for the component.
    
    https://trello.com/c/eKtXGtBD

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,1 +1,2 @@
 //= require child-benefit-tax-calculator
+//= require govuk_publishing_components/components/feedback
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Calculators::Application.routes.draw do
+  mount GovukPublishingComponents::Engine, at: "/component-guide"
+
   with_options format: false do |routes|
     routes.get "/child-benefit-tax-calculator/main" => "child_benefit_tax#main"
     routes.get "/child-benefit-tax-calculator/main/process_form" => "child_benefit_tax#process_form"
