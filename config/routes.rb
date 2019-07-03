92862a52c91139104479f56ddf19d9df80ebd900
Tijmen Brommet <tijmen@gmail.com>
2019-07-03 12:39:15 +0100

    Redirect to the only page from root
    This redirects `/` to `/child-benefit-tax-calculator/main` (the start
    page).
    
    This makes developing a little easier, because the current root throws
    up an error.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Calculators::Application.routes.draw do
   mount GovukPublishingComponents::Engine, at: "/component-guide"
 
+  root to: redirect("/child-benefit-tax-calculator/main")
+
   with_options format: false do |routes|
     routes.get "/child-benefit-tax-calculator/main" => "child_benefit_tax#main"
     routes.get "/child-benefit-tax-calculator/main/process_form" => "child_benefit_tax#process_form"
