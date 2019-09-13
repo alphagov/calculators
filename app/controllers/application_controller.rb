9c013ecae42e6652faa6f93c7631837c978734d4
Simon Hughesdon <simon.hughesdon@digital.cabinet-office.gov.uk>
2019-09-13 10:27:30 +0100

    Add basic auth to the review app
    This has been lifted from [Collections](https://github.com/alphagov/collections/blob/master/app/controllers/application_controller.rb#L11-L16)
    
    The env variables have been added to the review app already.
    
    This is to prevent users from accidentally landing on a page in the review
    app and believing that it's GOV.UK.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include Slimmer::Template
   slimmer_template 'core_layout'
+
+  if ENV["BASIC_AUTH_USERNAME"]
+    http_basic_authenticate_with(
+      name: ENV.fetch("BASIC_AUTH_USERNAME"),
+      password: ENV.fetch("BASIC_AUTH_PASSWORD")
+    )
+  end
 end
