cdc14ab8dd7de2b5cf532d64fb64b65cc0347795
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2019-10-29 17:28:55 +0000

    Use components for question 1
    - use fieldset component to wrap questions and use legend for question title instead of h2
    - style questions slightly indented to improve look of numbers next to questions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,20 @@
 
 module ApplicationHelper
   def step(num, text)
-    "<h2 class=\"step step-#{num}\">#{text}</h2>".html_safe
+    "<span class=\"step step-#{num}\">#{text}</span>".html_safe
   end
 
   def current_path_without_query_string
     request.original_fullpath.split("?", 2).first
+  end
+
+  def children_select_options
+    Array(1..10).map do |num|
+      {
+        text: num,
+        value: num,
+        selected: num == @calculator.children_count ? true : false
+      }
+    end
   end
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -3,7 +3,7 @@ require "spec_helper"
 describe ApplicationHelper, type: :helper do
   describe "#step" do
     it "generates the html for a step" do
-      expect(helper.step(1, "Blah")).to eq("<h2 class=\"step step-1\">Blah</h2>")
+      expect(helper.step(1, "Blah")).to eq("<span class=\"step step-1\">Blah</span>")
     end
   end
 
