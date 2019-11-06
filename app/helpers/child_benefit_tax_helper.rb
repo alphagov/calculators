c11acc0323705ed51c9cfc83ff8a5b0c2f7874cf
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2019-11-06 15:24:01 +0000

    Use components for question 4
    - removing the pound sign placeholder from these inputs because placeholders are bad, the component doesn't support them and when the page is submitted a pound sign is added into the input anyway

--- app/helpers/child_benefit_tax_helper.rb
+++ app/helpers/child_benefit_tax_helper.rb
@@ -5,6 +5,10 @@ module ChildBenefitTaxHelper
     text_field_tag(name, number_to_currency((amount.blank? || amount <= 0 ? nil : amount), unit: "£", precision: 2), { placeholder: "£" }.merge(options))
   end
 
+  def money_input_value(amount)
+    number_to_currency((amount.blank? || amount <= 0 ? nil : amount), unit: "£", precision: 2)
+  end
+
   def tax_year_label(year)
     dates = ChildBenefitTaxCalculator::TAX_YEARS[year.to_s]
     "#{dates.first.year} to #{dates.last.year}"
