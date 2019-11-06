93526c9c1cba10203bef4501b464fca4062c90d8
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2019-11-06 15:43:30 +0000

    Use components for question 5

--- app/helpers/child_benefit_tax_helper.rb
+++ app/helpers/child_benefit_tax_helper.rb
@@ -1,10 +1,6 @@
 # encoding: utf-8
 
 module ChildBenefitTaxHelper
-  def money_input(name, amount, options = {})
-    text_field_tag(name, number_to_currency((amount.blank? || amount <= 0 ? nil : amount), unit: "£", precision: 2), { placeholder: "£" }.merge(options))
-  end
-
   def money_input_value(amount)
     number_to_currency((amount.blank? || amount <= 0 ? nil : amount), unit: "£", precision: 2)
   end
