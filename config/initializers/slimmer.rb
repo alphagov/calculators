63cc5ec066ad82bdcf1c0e930c95433666aea6b7
Kevin Dew <kevindew@me.com>
2020-05-22 15:26:10 +0100

    Remove redundant slimmer configuration
    This is the default configuration.

--- config/initializers/slimmer.rb
+++ config/initializers/slimmer.rb
@@ -1,7 +1,3 @@
 Calculators::Application.configure do
   config.slimmer.logger = Rails.logger
-
-  if Rails.env.development?
-    config.slimmer.asset_host = Plek.current.find("static") || "http://static.dev.gov.uk"
-  end
 end
