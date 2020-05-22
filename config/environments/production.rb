82288f3d7395e4e5732f5fd6f3204e0daab39ba3
Kevin Dew <kevindew@me.com>
2020-05-22 15:48:29 +0100

    Remove dummy asset host config
    Since this is configured in config/application.rb it seemed a little
    fishy to have this comment here. I'm probably being a bit paranoid here
    as this might be the case with a bunch of commented out config
    🤷‍♂️

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,9 +31,6 @@ Calculators::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
-  # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = 'http://assets.example.com'
-
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
