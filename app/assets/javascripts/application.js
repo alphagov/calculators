18be3526bdac989c59b84d4a457098b44808b16a
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2020-06-17 10:32:54 +0100

    Use individual component JS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,7 @@
+//= require govuk_publishing_components/lib
+//= require govuk_publishing_components/components/button
+//= require govuk_publishing_components/components/error-summary
+//= require govuk_publishing_components/components/feedback
+//= require govuk_publishing_components/components/govspeak
+//= require govuk_publishing_components/components/radio
 //= require child-benefit-tax-calculator
-//= require govuk_publishing_components/all_components
